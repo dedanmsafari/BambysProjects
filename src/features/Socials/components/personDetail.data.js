50e27a5c5e673520bb94c9f17a46f89663428fac
dedanmsafari <dedan.msafari@gmail.com>
2022-12-10 10:10:51 +0300

    personaDetails Data

---
+++ src/features/Socials/components/personDetail.data.js
@@ -0,0 +1,64 @@
+const PersonData = [
+  {
+    id: 1,
+    name: "Angela Smith",
+    source: require("../../../../assets/woman1.jpg"),
+    postNumber: 60,
+  },
+  {
+    id: 2,
+    name: "Mark Duke.",
+    source: require("../../../../assets/man2.jpg"),
+    postNumber: 47,
+  },
+  {
+    id: 3,
+    name: "Susan Drew.",
+    source: require("../../../../assets/woman2.jpg"),
+    postNumber: 33,
+  },
+  {
+    id: 4,
+    name: "Clara M.",
+    source: require("../../../../assets/smilingWoman.jpg"),
+    postNumber: 21,
+  },
+  {
+    id: 5,
+    name: "Tommy Lee",
+    source: require("../../../../assets/man1.jpg"),
+    postNumber: 15,
+  },
+  {
+    id: 6,
+    name: "Angela Smith",
+    source: require("../../../../assets/woman1.jpg"),
+    postNumber: 60,
+  },
+  {
+    id: 7,
+    name: "Mark Duke.",
+    source: require("../../../../assets/man2.jpg"),
+    postNumber: 47,
+  },
+  {
+    id: 8,
+    name: "Susan Drew.",
+    source: require("../../../../assets/woman2.jpg"),
+    postNumber: 33,
+  },
+  {
+    id: 9,
+    name: "Clara M.",
+    source: require("../../../../assets/smilingWoman.jpg"),
+    postNumber: 21,
+  },
+  {
+    id: 10,
+    name: "Tommy Lee",
+    source: require("../../../../assets/man1.jpg"),
+    postNumber: 15,
+  },
+];
+
+export default PersonData;
