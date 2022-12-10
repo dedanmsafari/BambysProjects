692ff39a58a668aadefed08dabc98cf8e2dafa41
dedanmsafari <dedan.msafari@gmail.com>
2022-12-10 10:10:37 +0300

    personalDetail component

---
+++ src/features/Socials/components/personDetail.component.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { View } from "react-native";
+import AvatarIcon from "../../../components/Avatar/avatar.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "../../../components/text/text.component";
+import {
+  PersonContainer,
+  UserContainer,
+  PostContainer,
+  IconContainer,
+} from "../screens/screens.styles";
+import { Ionicons } from "@expo/vector-icons";
+import { colors } from "../../../infrastructure/theme/colors";
+
+const PersonDetail = ({ info: { source, name, postNumber } }) => {
+  return (
+    <PersonContainer>
+      <UserContainer>
+        <AvatarIcon source={source} online />
+        <Spacer position="right" size="small" />
+        <Text>{name}</Text>
+      </UserContainer>
+      <PostContainer>
+        <IconContainer>
+          <Ionicons name="bookmarks" size={24} color={colors.brand.primary} />
+
+          <Text> {postNumber} posts</Text>
+        </IconContainer>
+      </PostContainer>
+    </PersonContainer>
+  );
+};
+
+export default PersonDetail;
