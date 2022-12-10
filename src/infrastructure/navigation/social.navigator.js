cb536f0075ca250fa6c2233bcf2c8bd0539c38ec
dedanmsafari <dedan.msafari@gmail.com>
2022-12-10 10:11:45 +0300

    social navigator

---
+++ src/infrastructure/navigation/social.navigator.js
@@ -0,0 +1,32 @@
+import { View } from "react-native";
+
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import ArrivalsScreen from "../../features/Socials/screens/arrivals.screens";
+import LocalsScreen from "../../features/Socials/screens/locals.screens";
+import { StyledSafeAreaView } from "../../utils/safeArea.util.component";
+import { colors } from "../theme/colors";
+
+const Tab = createMaterialTopTabNavigator();
+
+function SocialNavigator() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <StyledSafeAreaView>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarLabelStyle: { fontSize: 15, fontWeight: "500" },
+            tabBarActiveTintColor: `${colors.brand.tertiary}`,
+            tabBarIndicatorStyle: {
+              backgroundColor: `${colors.brand.tertiary}`,
+            },
+          }}
+        >
+          <Tab.Screen name="Locals" component={LocalsScreen} />
+          <Tab.Screen name="Arrivals" component={ArrivalsScreen} />
+        </Tab.Navigator>
+      </StyledSafeAreaView>
+    </View>
+  );
+}
+
+export default SocialNavigator;
