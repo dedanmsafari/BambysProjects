6ba86bfe395dbfebd9ba243ed2b39bd06f5284e5
dedanmsafari <dedan.msafari@gmail.com>
2022-12-12 10:00:42 +0300

    radius distance component

---
+++ src/features/Profile/components/radiusDistance.component.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { SliderArea, SliderContainer } from "../screens/profileScreen.styles";
+import { Slider } from "@miblanchard/react-native-slider";
+import { Text } from "../../../components/text/text.component";
+import { colors } from "../../../infrastructure/theme/colors";
+import { Spacer } from "../../../components/spacer/spacer.component";
+
+const RadiusDistanceBlock = () => {
+  const [value, setValue] = useState(0);
+
+  // console.log(Math.round(value));
+
+  const thumbComponent = () => {
+    return <Text variant="caption">{Math.round(value * 10) / 10} Kms</Text>;
+  };
+
+  return (
+    <SliderContainer>
+      <Text>Radius</Text>
+      <Spacer />
+      <SliderArea>
+        <Slider
+          value={value}
+          onValueChange={(value) => setValue(value)}
+          minimumTrackTintColor={colors.brand.tertiary}
+          thumbTintColor={colors.brand.tertiary}
+          maximumValue={10}
+          minimumValue={0}
+          renderAboveThumbComponent={thumbComponent}
+        />
+      </SliderArea>
+    </SliderContainer>
+  );
+};
+
+export default RadiusDistanceBlock;
