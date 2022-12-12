27d28f8c9a2773af062e90649d082be35243d16b
dedanmsafari <dedan.msafari@gmail.com>
2022-12-12 09:59:24 +0300

    topbar component changes

--- src/features/Home/components/TopBar/topbar.component.js
+++ src/features/Home/components/TopBar/topbar.component.js
@@ -1,27 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Image } from "react-native";
 import {
   TopBarContainer,
   LogoButtonHome,
   IconContainer,
   IconButton,
+  ModalContainer,
 } from "./topbar.styles";
 import { Ionicons } from "@expo/vector-icons";
 import { colors } from "../../../../infrastructure/theme/colors";
 
-const TopBar = () => {
+const TopBar = ({ onPress }) => {
   return (
     <TopBarContainer>
-      <LogoButtonHome activeOpacity={0.5}>
+      <LogoButtonHome activeOpacity={0.5} onPress={onPress}>
         <Image
           style={{ height: 35, width: 130 }}
           source={require("../../../../../assets/brandName.png")}
         />
         <Ionicons name="chevron-down-outline" size={20} color="black" />
       </LogoButtonHome>
+
       <IconContainer>
         <IconButton>
-          <Ionicons name="search" size={20} color={colors.brand.primary} />
+          <Ionicons name="map-sharp" size={20} color={colors.brand.primary} />
         </IconButton>
         <IconButton>
           <Ionicons
