5f922180b4dc0726e89aa29b1a22dd420ddb26d0
dedanmsafari <dedan.msafari@gmail.com>
2022-12-13 10:50:46 +0300

    header styles component

---
+++ src/features/Home/components/header/header.styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.View`
+  height: 50px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 15px;
+`;
+
+export const BackIconContainer = styled.TouchableOpacity`
+  flex-direction: row;
+`;
+export const MoreVertContainer = styled.TouchableOpacity`
+  flex-direction: row;
+  margin-left: auto;
+`;
