c8c85c2312f46441b296d391dd06cf0da6be896b
dedanmsafari <dedan.msafari@gmail.com>
2022-12-16 18:04:46 +0300

    onBoarding styles

---
+++ src/features/Account/onBoarding/onBoarding.styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+// --- Neighborhood Styles
+
+export const NeighborhoodContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+// -- Neighborhood Styles
+
+export const ScreenContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  padding-top: 50px;
+`;
+
+// -- Travel Styles
+export const TravelContainer = styled.View`
+  flex: 1;
+  align-items: center;
+`;
