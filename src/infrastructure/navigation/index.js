331edd167ec398c75b6bc828b875ae7d9c2b6603
dedanmsafari <dedan.msafari@gmail.com>
2022-12-22 00:03:19 +0300

    minor changes to index

--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -1,15 +1,16 @@
 import React, { useContext } from "react";
 import AppNavigator from "./app.navigator";
-// import { AccountNavigator } from "./account.navigator";
-// import { AuthenticationContext } from "../../services/authentication/authentication.context";
+import AccountNavigator from "./account.navigator";
 import { NavigationContainer } from "@react-navigation/native";
+import { AuthenticationContext } from "../../services/authentication/authentication.context";
 
 export const Navigation = ({ onReady }) => {
-  // const { isAuthenticated } = useContext(AuthenticationContext);
+  const { loggedIn } = useContext(AuthenticationContext);
+
   return (
     <NavigationContainer onReady={onReady}>
-      {/* {isAuthenticated ? <AppNavigator /> : <AccountNavigator />} */}
-      <AppNavigator />
+      {loggedIn ? <AppNavigator /> : <AccountNavigator />}
+      {/* <AppNavigator /> */}
     </NavigationContainer>
   );
 };
